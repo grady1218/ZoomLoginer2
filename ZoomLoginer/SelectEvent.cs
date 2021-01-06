52e497d487d9373d920be0023953a3b72d3d3db6
grady1218 <isikawaseizi810@gmail.com>
2021-01-06 17:55:15 +0900

    aaaa

---
+++ ZoomLoginer/SelectEvent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace ZoomLoginer
+{
+    class SelectEvent : DataGridView
+    {
+        public SelectEvent()
+        {
+            AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            AllowUserToResizeColumns = false;
+            AllowUserToResizeRows = false;
+
+            ColumnCount = 3;
+            
+            Columns[0].HeaderText = "イベント名";
+            Columns[1].HeaderText = "URL";
+            Columns[2].HeaderText = "時間";
+
+        }
+
+    }
+}
--- ZoomLoginer/SetScheduleForm.cs
+++ ZoomLoginer/SetScheduleForm.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ZoomLoginer
 {
     class SetScheduleForm : BaseForm
     {
+        PaintEventArgs paintEventArgs;
         public string WeekDay { get; set; }
+
+        SelectEvent SelectEvent;
         public SetScheduleForm()
         {
+            SelectEvent = new SelectEvent()
+            {
+                Size = new System.Drawing.Size(475,300),
+                Location = new System.Drawing.Point(50, 150),
+                BackColor = System.Drawing.Color.FromArgb(60, 60, 60)
+            };
 
+            Controls.Add(SelectEvent);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            paintEventArgs = e;
         }
     }
 
